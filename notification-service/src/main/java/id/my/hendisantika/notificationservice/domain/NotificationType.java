61946cfe7b5045b9a71095213e3d15b9ded7746e
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:32:58 +0700

    feat: Add NotificationType🫸🌀✏️📗🐧🐳

---
+++ notification-service/src/main/java/id/my/hendisantika/notificationservice/domain/NotificationType.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.notificationservice.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.32
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationType {
+    PAYMENT_SUCCESS,
+    PAYMENT_FAILED,
+    PAYMENT_TIMEOUT
+}
