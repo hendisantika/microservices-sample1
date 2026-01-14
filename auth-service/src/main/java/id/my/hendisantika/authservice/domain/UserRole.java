13b3c5856c5a2bbbda30eac7f362d9af847ffd47
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:17:44 +0700

    feat: Add UserRole Domain🫸🌀✏️📗🐧🐳

---
+++ auth-service/src/main/java/id/my/hendisantika/authservice/domain/UserRole.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.authservice.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.17
+ * To change this template use File | Settings | File Templates.
+ */
+public enum UserRole {
+    USER,
+    ADMIN
+}
