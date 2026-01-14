631911a5831fdeca8770573964e201eb15406b82
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:20:07 +0700

    feat: Add login in AuthController🫸🌀✏️📗🐧🐳

--- auth-service/src/main/java/id/my/hendisantika/authservice/controller/AuthController.java
+++ auth-service/src/main/java/id/my/hendisantika/authservice/controller/AuthController.java
@@ -1,8 +1,16 @@
 package id.my.hendisantika.authservice.controller;
 
+import id.my.hendisantika.authservice.domain.LoginRequest;
+import id.my.hendisantika.authservice.domain.UserRole;
 import id.my.hendisantika.authservice.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,8 +29,38 @@ public class AuthController {
 
     private final AuthService authService;
 
-
     public AuthController(AuthService authService) {
         this.authService = authService;
+    }
+
+    @PostMapping("/login")
+    public Map<String, String> login(@RequestBody LoginRequest request) {
+        if ("admin".equals(request.username())
+                && "admin123".equals(request.password())) {
+
+            return Map.of(
+                    "access_token",
+                    authService.generateToken(
+                            request.username(),
+                            UserRole.ADMIN
+                    )
+            );
+        }
+        if ("user".equals(request.username())
+                && "user123".equals(request.password())) {
+
+            return Map.of(
+                    "access_token",
+                    authService.generateToken(
+                            request.username(),
+                            UserRole.USER
+                    )
+            );
+        }
+
+        throw new ResponseStatusException(
+                HttpStatus.UNAUTHORIZED,
+                "Invalid credentials"
+        );
     }
 }
