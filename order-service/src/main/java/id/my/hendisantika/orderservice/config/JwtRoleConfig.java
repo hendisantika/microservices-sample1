46707098463464e40f78baf1d5da5284c8f055f2
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:37:09 +0700

    feat: Add JwtRoleConfig🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/config/JwtRoleConfig.java
@@ -0,0 +1,38 @@
+package id.my.hendisantika.orderservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.36
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class JwtRoleConfig {
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+
+        JwtGrantedAuthoritiesConverter authoritiesConverter =
+                new JwtGrantedAuthoritiesConverter();
+
+        authoritiesConverter.setAuthoritiesClaimName("roles");
+        authoritiesConverter.setAuthorityPrefix("");
+
+        JwtAuthenticationConverter converter =
+                new JwtAuthenticationConverter();
+
+        converter.setJwtGrantedAuthoritiesConverter(authoritiesConverter);
+
+        return converter;
+    }
+}
