bdea68cffb9341068c9425de394cf41a829fab7b
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 06:06:49 +0700

    feat: Add PaymentStatus Domain🫸🌀✏️📗🐧🐳

---
+++ payment-service/src/main/java/id/my/hendisantika/paymentservice/domain/PaymentStatus.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.paymentservice.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 06.06
+ * To change this template use File | Settings | File Templates.
+ */
+public enum PaymentStatus {
+    SUCCESS,
+    FAILED
+}
