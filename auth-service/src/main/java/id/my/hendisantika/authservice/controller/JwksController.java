407268af6ca26c729d921b0fd3b4d7a5e72dfd16
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:20:33 +0700

    feat: Add JwksController🫸🌀✏️📗🐧🐳

---
+++ auth-service/src/main/java/id/my/hendisantika/authservice/controller/JwksController.java
@@ -0,0 +1,27 @@
+package id.my.hendisantika.authservice.controller;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.20
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/auth/.well-known")
+public class JwksController {
+
+    private final JWKSet jwkSet;
+
+    public JwksController(JWKSet jwkSet) {
+        this.jwkSet = jwkSet;
+    }
+}
