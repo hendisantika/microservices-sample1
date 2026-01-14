275ead9687489495ffc0fa3245e35f4f83042701
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:44:14 +0700

    feat: Add doFilterInternal in IdempotencyFilter🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/orderservice/idempotency/IdempotencyFilter.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/idempotency/IdempotencyFilter.java
@@ -1,7 +1,15 @@
 package id.my.hendisantika.orderservice.idempotency;
 
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,5 +29,41 @@ public class IdempotencyFilter extends OncePerRequestFilter {
 
     public IdempotencyFilter(IdempotencyStore store) {
         this.store = store;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (!"POST".equals(request.getMethod())
+                || !"/orders".equals(request.getRequestURI())) {
+
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String key = request.getHeader("Idempotency-Key");
+        if (key == null || key.isBlank()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        store.findExisting(key).ifPresent(existing -> {
+            try {
+                response.setStatus(HttpStatus.OK.value());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                response.getWriter().write("""
+                            {"orderId":"%s"}
+                        """.formatted(existing.getId()));
+                response.flushBuffer();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        if (response.isCommitted()) {
+            return;
+        }
+
+
+        filterChain.doFilter(request, response);
     }
 }
