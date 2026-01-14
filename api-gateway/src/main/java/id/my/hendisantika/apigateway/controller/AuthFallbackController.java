c2be549f2c32609c9cf1672e4e4bca0fc5f4ca8e
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:10:13 +0700

    feat: Add AuthFallbackController🫸🌀✏️📗🐧🐳

---
+++ api-gateway/src/main/java/id/my/hendisantika/apigateway/controller/AuthFallbackController.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.apigateway.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.09
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("fallback/auth")
+public class AuthFallbackController {
+
+}
