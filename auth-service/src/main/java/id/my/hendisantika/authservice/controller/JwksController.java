c29ce8d5129771cd2f33208f35294ecdff59156a
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:21:10 +0700

    feat: Add keys in JwksController🫸🌀✏️📗🐧🐳

--- auth-service/src/main/java/id/my/hendisantika/authservice/controller/JwksController.java
+++ auth-service/src/main/java/id/my/hendisantika/authservice/controller/JwksController.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.authservice.controller;
 
+import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,5 +27,15 @@ public class JwksController {
 
     public JwksController(JWKSet jwkSet) {
         this.jwkSet = jwkSet;
+    }
+
+    @GetMapping("/jwks.json")
+    public Map<String, Object> keys() {
+        return new JWKSet(
+                jwkSet.getKeys()
+                        .stream()
+                        .map(JWK::toPublicJWK)
+                        .toList()
+        ).toJSONObject();
     }
 }
