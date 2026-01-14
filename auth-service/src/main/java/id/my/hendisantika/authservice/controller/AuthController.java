3e457d20ece27c7e834c0a396a567d238f920a1c
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:19:21 +0700

    feat: Add AuthController🫸🌀✏️📗🐧🐳

---
+++ auth-service/src/main/java/id/my/hendisantika/authservice/controller/AuthController.java
@@ -0,0 +1,28 @@
+package id.my.hendisantika.authservice.controller;
+
+import id.my.hendisantika.authservice.service.AuthService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.18
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+}
