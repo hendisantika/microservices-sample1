c2f457be64e51793d3c8c684a7c842e86cf8bf99
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:22:12 +0700

    feat: Add dlt-replay-service🫸🌀✏️📗🐧🐳

---
+++ dlt-replay-service/src/main/java/id/my/hendisantika/dltreplayservice/DltReplayServiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.dltreplayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DltReplayServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(DltReplayServiceApplication.class, args);
+    }
+
+}
