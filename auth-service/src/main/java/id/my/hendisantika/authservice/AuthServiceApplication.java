0ebd3860bcbcd9c3bcc0ffc5aff7da4bed0d8b6e
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:12:58 +0700

    feat: Add auth-service🫸🌀✏️📗🐧🐳

---
+++ auth-service/src/main/java/id/my/hendisantika/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
