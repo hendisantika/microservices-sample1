4fcdfbbdc686318f07223cf1a17e9ae0d66aac46
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:39:30 +0700

    feat: Add creatOrder in OrderController🫸🌀✏️📗🐧🐳

--- order-service/src/main/java/id/my/hendisantika/orderservice/controller/OrderController.java
+++ order-service/src/main/java/id/my/hendisantika/orderservice/controller/OrderController.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.orderservice.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 /**
  * Created by IntelliJ IDEA.
@@ -22,5 +28,13 @@ public class OrderController {
 
     public OrderController(OrderService service) {
         this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<UUID> creatOrder(
+            @RequestHeader("Idempotency-Key") String key,
+            @RequestHeader("X-User-Id") String userId
+    ) {
+        return new ResponseEntity<>(service.createOrder(userId, key), HttpStatus.OK);
     }
 }
