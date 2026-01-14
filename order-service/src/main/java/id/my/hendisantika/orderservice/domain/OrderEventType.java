771a2d66137ee1b9056394c6faf042586b485eb5
hendisantika <hendisantika@yahoo.co.id>
2026-01-15 05:40:56 +0700

    feat: Add OrderEventType Domain🫸🌀✏️📗🐧🐳

---
+++ order-service/src/main/java/id/my/hendisantika/orderservice/domain/OrderEventType.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.orderservice.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : microservices-sample1
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 15/01/26
+ * Time: 05.40
+ * To change this template use File | Settings | File Templates.
+ */
+public enum OrderEventType {
+    ORDER_CREATED,
+    ORDER_PAYMENT_TIMEOUT
+}
